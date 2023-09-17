65b1b767ace2d8a5ffec22a04b460f1f23be09ca
문광희 <mgh7020@gmail.com>
2023-09-17 22:54:59 +0900

    MGOS Project - Dot Coloseum 1st Commit

---
+++ src/main/java/webapp/mgos/controller/TestController.java
@@ -0,0 +1,15 @@
+package webapp.mgos.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TestController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index.html";
+    }
+}
---
+++ src/test/java/webapp/mgos/MGosApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.mgos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MGosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
